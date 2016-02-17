1946a1442eb1d065f03d6541617e022ea6298a85
Emma Weil <eweil@college.harvard.edu>
2016-02-16 22:02:29 -0500

    first commit

---
+++ point1.c
@@ -0,0 +1,40 @@
+// create graphs that are matrices
+
+// randomly generate a matrix with n vertices with distances between [0,1]
+
+// randomly generate a set of n points all within a unit square
+// then use those points to find a distance between them and put in
+// adjacency matrix
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+typedef unsigned long UL;
+
+int main(void)
+{
+	double rand_max = RAND_MAX;
+	srand((int)time(NULL));
+
+	int n = 16;
+	double throwaway = rand() / rand_max;
+
+	double weights[n];
+	for(int i = 1 )
+	{
+		
+	}
+	
+	
+	printf("%f, %f, %f\n", r1,r2,r3);
+//	UL[] g = graph0(r);
+}
+
+
+//UL[] graph0(int n)
+//{
+
+//}
+
