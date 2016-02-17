9fcc5d6ce55b247f5cfff6b36992abfc83c11839
James Baskerville <baskerville@college.harvard.edu>
2016-02-17 01:12:01 -0500

    added support for multiple trials (and fixed pcg stuff)

--- randmst.c
+++ randmst.c
@@ -47,45 +47,49 @@ int main(int argc, char *argv[]) {
     pcg32_random_t rng1;
     pcg32_srandom_r(&rng1, time(NULL), (intptr_t)&rng1);
     
-    srand(time(NULL));
+    for (int trial; trial < trials; trial++) {
 
-    // weights of edges chosen uniformly at random on [0,1]
-    if (dim == 0) {
-        for (int i = 0; i < n * n; i++) {
-            A[i] = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+        // weights of edges chosen uniformly at random on [0,1]
+        if (dim == 0) {
+            for (int i = 0; i < n * n; i++) {
+                A[i] = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+            }
         }
-    }
-    // weights of edges are Euclidean distances between connected vertices
-    else {
-        for (int i = 0; i < n; i++) {
-            Node* newnode = (Node*) calloc(1, sizeof(Node));
-            newnode->id = i;
-            if (dim == 2) {
-                newnode->x1 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
-                newnode->x2 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+        // weights of edges are Euclidean distances between connected vertices
+        else {
+            for (int i = 0; i < n; i++) {
+                Node* newnode = (Node*) calloc(1, sizeof(Node));
+                newnode->id = i;
+                if (dim == 2) {
+                    newnode->x1 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+                    newnode->x2 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+                }
+                else if (dim == 3) {
+                    newnode->x3 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+                }
+                else if (dim == 4) {
+                    newnode->x4 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+                }
+                M[i] = newnode;
             }
-            else if (dim == 3) {
-                newnode->x3 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+
+            // create adjacency matrix
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    float res = dist(M[j], M[k]);
+                    A[n*j + k] = res;
+                    printf("%f   ", res);
+                }
+                printf("\n");
             }
-            else if (dim == 4) {
-                newnode->x4 = (float) pcg32_random_r(&rng1) / UINT32_MAX;
+
+            for (int i = 0; i < n; i++) {
+                free(M[i]);
             }
-            M[i] = newnode;
         }
 
-        // create adjacency matrix
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                float res = dist(M[j], M[k]);
-                A[n*j + k] = res;
-                printf("%f   ", res);
-            }
-            printf("\n");
-        }
+        printf("done with trial %d\n\n", trial);
 
-        for (int i = 0; i < n; i++) {
-            free(M[i]);
-        }
     }
 
     // find MST
